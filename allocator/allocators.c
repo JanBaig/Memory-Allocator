393a04f3664bcf095709df3e321b2e9152f7e8df
Janat Baig <janatbaig2@gmail.com>
2023-06-06 20:56:18 -0400

    tweaks

--- allocator/allocators.c
+++ allocator/allocators.c
@@ -5,45 +5,6 @@
 #define METADATA_SIZE 20
 block* base = NULL; // starting point of the heap
 
-block* extend_heap(block* last, size_t s) {
-	
-	int sb;
-	block* b;
-	b = sbrk (0);
-	sb = (int)sbrk( METADATA_SIZE + s);
-	if (sb < 0) { return (NULL); }
-	b->size = s;
-	b->next = NULL;
-	b->prev = last;
-	b->ptr = b->data;
-	if (last) { last ->next = b; }
-	b->free = 0;
-	return (b);
-}
-
-block* find_block(block* last, size_t size) {
-	block* b = base;
-	while (b && !(b->free && b->size >= size)) { 
-       		last = b;
-		b = b->next;
-	}
-	return b;
-}
-
-void split_block(block* b, size_t s) {
-	
-	block* new;
-	new = (block*)(b->data + s); // starting address of the new block - b->data is the end of metadata 
-	new->size = b->size - s - METADATA_SIZE; // size of the new's data portion 
-	new->next = b->next;
-	new->prev = b;
-	new->free = 1;
-	new->ptr = new->data;
-	b->size = s;
-	b->next = new;
-	if (new->next) { new->next->prev = new; }
-}
-
 void* mallocImp(size_t size) {
 	
 	block* b; 
@@ -51,14 +12,13 @@ void* mallocImp(size_t size) {
 	size_t s = align4(size);
 	
 	if (base) {
-		// Blocks already exist - need to find a free one 
+		// The base already exists - need to find a free one 
 		last = base;
 		b = find_block(last, s);
 		if (b) {
 			// Check if we can split it 
 			if ((b->size - s) >= (METADATA_SIZE + 4)) { split_block(b, s); }
 			b->free = 0;
-
 		} else {
 			// No empty fitting block found - extend the heap
 			b = extend_heap(last, s);
@@ -66,7 +26,7 @@ void* mallocImp(size_t size) {
 		}
 	
 	} else {
-		// First block added to the heap
+		// No base - 'b' is the first block added to the heap
 		b = extend_heap(NULL, s);
 		if (!b) { return NULL; }
 		base = b; 
@@ -86,58 +46,6 @@ void* callocImp(size_t number, size_t size) {
 		}
 	}
 	return new;
-}
-
-block* fusion(block* b) {
-	if (b->next && b->next->free) {  
-		b->size += METADATA_SIZE + b->next->size;
-		b->next = b->next->next; 
-		if (b->next) { b->next->prev = b; }
-	}	
-	return b;
-}
-
-block* get_block(void* p) {
-	char* tmp = p;
-	return p = tmp -= METADATA_SIZE;
-} 
-
-int valid_addr(void* p) {
-	// a blocks->ptr holds the start of the data, subtracting the metadata size from it means that we arrive
-	// at the start of the block and check to see if the argument p is equal to the returned address
-	if (base) {
-		if (p>base && p<sbrk(0)) { return p == (get_block(p))->ptr; }
-	}
-	return 0;
-}
-
-void freeImp(void* p) {
-	block* b;
-	if (valid_addr(p)) {
-		b = get_block(p);
-		b->free = 1;
-		// Fuse with the previous block if possible
-		if (b->prev && b->prev->free) { b = fusion(b->prev); }
-		if (b->next) { fusion(b); }
-		else {
-			// b does not have a next - we are at the end of the heap
-			if (b->prev) { b->prev->next = NULL; }
-			else { base = NULL; } // if no prev, there is only one block and we free that 
-			brk(b);
-		}
-	}
-}
-
-void copy_block(block* src, block* dst) {
-	int* sdata;
-	int* ddata;
-	size_t i;
-
-	sdata = src->ptr;
-	ddata = dst->ptr;
-	for (i=0; (i*4 < src->size) && (i*4 < dst->size); i++) {
-		ddata[i] = sdata[i];
-	}
 }
 
 void* reallocImp(void* p, size_t size) {
@@ -175,5 +83,95 @@ void* reallocImp(void* p, size_t size) {
 	return NULL;
 }
 
+void freeImp(void* p) {
+	block* b;
+	if (valid_addr(p)) {
+		b = get_block(p);
+		b->free = 1;
+		// Fuse with the previous block if possible
+		if (b->prev && b->prev->free) { b = fusion(b->prev); }
+		if (b->next) { fusion(b); }
+		else {
+			// b does not have a next - we are at the end of the heap
+			if (b->prev) { b->prev->next = NULL; }
+			else { base = NULL; } // if no prev, there is only one block and we free that 
+			brk(b);
+		}
+	}
+}
 
+// Helper Functions
+
+block* extend_heap(block* last, size_t s) {
+	
+	block* b = sbrk(0); // address returned will hold a block
+	int sb = (int)sbrk( METADATA_SIZE + s);
+
+	if (sb == -1) { return (NULL); } // error returned
+	b->size = s;
+	b->next = NULL;
+	b->prev = last;
+	b->ptr = b->data;
+	if (last) { last->next = b; }
+	b->free = 0;
+	return b;
+}
+
+block* find_block(block* last, size_t size) {
+	block* b = base;
+	while (b && !(b->free && b->size >= size)) { 
+       	last = b;
+		b = b->next;
+	}
+	return b;
+}
+
+void split_block(block* b, size_t s) {
+	
+	block* new;
+	new = (block*)(b->data + s); // starting address of the new block - b->data is the end of metadata 
+	new->size = b->size - s - METADATA_SIZE; // size of the new's data portion 
+	new->next = b->next;
+	new->prev = b;
+	new->free = 1;
+	new->ptr = new->data;
+	b->size = s;
+	b->next = new;
+	if (new->next) { new->next->prev = new; }
+}
+
+block* fusion(block* b) {
+	if (b->next && b->next->free) {  
+		b->size += METADATA_SIZE + b->next->size;
+		b->next = b->next->next; 
+		if (b->next) { b->next->prev = b; }
+	}	
+	return b;
+}
+
+block* get_block(void* p) {
+	char* tmp = p;
+	return p = tmp -= METADATA_SIZE;
+} 
+
+int valid_addr(void* p) {
+	// a blocks->ptr holds the start of the data, subtracting the metadata size from it means that we arrive
+	// at the start of the block and check to see if the argument p is equal to the returned address
+	if (base) {
+		if (p>base && p<sbrk(0)) { return p == (get_block(p))->ptr; }
+	}
+	return 0;
+}
+
+void copy_block(block* src, block* dst) {
+	int* sdata;
+	int* ddata;
+	size_t i;
+
+	sdata = src->ptr;
+	ddata = dst->ptr;
+	for (i=0; (i*4 < src->size) && (i*4 < dst->size); i++) {
+		ddata[i] = sdata[i];
+	}
+}
 
--- allocator/allocators.h
+++ allocator/allocators.h
@@ -5,18 +5,20 @@ struct block {
 	size_t size; // size of the data area - not the whole block's size 
 	block* next;
 	block* prev;
-	int free;
-	void* ptr;
-	char data [1]; // indicates the end of the metadata
+	int free; // 1 if free, 0 is not
+	void* ptr; // points to where data segment of block starts (size attribute)
+	char data [1]; // indicates the end of the metadata, address of start of data
 };
 
 void* mallocImp(size_t size);
 
 void* callocImp(size_t number, size_t size);
 
+void* reallocImp(void* p, size_t size);
+
 void freeImp(void* p);
 
-void* reallocImp(void* p, size_t size);
+// Helper Functions
 
 block* fusion(block* b);
 
@@ -31,5 +33,4 @@ block* extend_heap(block* last, size_t s);
 block* find_block(block* last, size_t size); 
 
 void split_block(block* b, size_t s); 
-
 
